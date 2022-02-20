bd19391ca55d935a84ecfef31d8e702976470986
DanielPomfyuk <danyapomfyuk99@gmail.com>
2022-02-20 14:14:26 +0000

    refactor the code

--- OnboardingScreen.js
+++ OnboardingScreen.js
@@ -4,17 +4,12 @@ import DATA from './assets/login_screen_data.js'
 import colours from './colours.js'
 const { width, height } = Dimensions.get('screen');
 
-// https://www.flaticon.com/packs/retro-wave
-// inspiration: https://dribbble.com/shots/11164698-Onboarding-screens-animation
-// https://twitter.com/mironcatalin/status/1321180191935373312
-
-const bgs = [colours.BACKGROUND, colours.BACKGROUND, colours.BACKGROUND, colours.BACKGROUND];
 const Indicator = ({ scrollX }) => {
   return (
-    <View style={{ position: 'absolute', bottom: 200, flexDirection: 'row' }}>
+    <View style={{ position: 'absolute', bottom: 75, flexDirection: 'row' }}>
       {DATA.map((_, i) => {
         const inputRange = [(i - 1) * width, i * width, (i + 1) * width];
-
+        
         const scale = scrollX.interpolate({
           inputRange,
           outputRange: [0.8, 1.4, 0.8],
@@ -28,83 +23,56 @@ const Indicator = ({ scrollX }) => {
         });
 
         return (<Animated.View
-        key={`indicator-${i}`}
-        style={{
-          height: 10, 
-          width: 10, 
-          borderRadius: 5, 
-          backgroundColor: '#fff', 
-          margin: 10, 
-          transform: [
-            {
-               scale, 
-            }
-          ]
-        }}
-      />
-      );
-    })}
-  </View>
+          key={`indicator-${i}`}
+          style={[styles.indicatorDot,{
+            opacity,
+            transform: [{scale}]
+          }]}
+        />
+        );
+      })}
+    </View>
   );
 };
 
-const Backdrop = ({ scrollX }) => {
-  const backgroundColor = scrollX.interpolate({
-    inputRange: bgs.map((_, i) => i * width),
-    outputRange: bgs.map((bg) => bg)
-  })
+const Backdrop = () => {
   return <Animated.View
-    style={[StyleSheet.absoluteFillObject, { backgroundColor }]}
+    style={[StyleSheet.absoluteFillObject, { backgroundColor: colours.BACKGROUND }]}
   />
 }
 
-const Square = ({scrollX})=>{
+const Square = ({ scrollX }) => {
   const YOLO = Animated.modulo(Animated.divide(
-    Animated.modulo(scrollX,width),
+    Animated.modulo(scrollX, width),
     new Animated.Value(width)
-  ),1)
+  ), 1)
 
   const rotate = YOLO.interpolate({
-    inputRange: [0,0.5,1],
-    outputRange: ['35deg','0deg','35deg']
+    inputRange: [0, 0.5, 1],
+    outputRange: ['35deg', '0deg', '35deg']
   })
 
   const translateX = YOLO.interpolate({
-    inputRange: [0,0.5,1],
+    inputRange: [0, 0.5, 1],
     outputRange: [0, -height, 0]
   })
+
   return <Animated.View
-    style={{width: height, 
-      height:height,
-      backgroundColor:colours.SQUARE,
-      borderRadius: 86, 
-      position:'absolute',
-      top:-height* 0.6,
-      left:-height*0.3,
-      transform:[
-        {
-          rotate
-        },
-        {
-          translateX
-        }
-      ]}}
+    style={[styles.square,{transform: [{rotate},{translateX}]}]}
   />
 }
 
-const Button = ({text}) =>{
-  return(
-    <TouchableOpacity style={styles.button}><Text style={styles.buttonText}>{text}</Text></TouchableOpacity>
-  )
+const Button = ({ text }) => {
+  return (<TouchableOpacity style={styles.button}><Text style={styles.buttonText}>{text}</Text></TouchableOpacity>)
 }
-export default function LoginScreen() {
+
+export default function OnboardingScreen() {
   const scrollx = React.useRef(new Animated.Value(0)).current;
-  const a = "Log In"
   return (
     <View style={styles.container}>
       <StatusBar hidden />
-      <Backdrop scrollX={scrollx} />
-      <Square scrollX={scrollx}/>
+      <Backdrop />
+      <Square scrollX={scrollx} />
       <Animated.FlatList
         data={DATA}
         keyExtractor={item => item.key}
@@ -119,19 +87,19 @@ export default function LoginScreen() {
         pagingEnabled
         renderItem={({ item }) => {
           return <View style={{ width, alignItems: 'center', padding: 20 }}>
-            <View style={{ flex: .7, justifyContent: 'center' }}>
+            <View style={{ flex: 0.7, justifyContent: 'center' }}>
               <Image source={item.image} style={{ width: width / 2, height: height / 2, resizeMode: 'contain' }} />
             </View>
             <View style={{ flex: 0.3 }}>
-              <Text style={{ fontWeight: '800', fontSize: 28, marginBottom: 10, color: colours.SQUARE }}>{item.title}</Text>
-              <Text style={{ fontWeight: '300', color: colours.SQUARE }}>{item.description}</Text>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.description}>{item.description}</Text>
             </View>
           </View>
         }}
       />
-      <View style={{ position: 'absolute', bottom: 75, flexDirection: 'row' }}>
-        <Button text={"Log In"}/>
-        <Button text={"Sign Up"}/>
+      <View style={{ position: 'absolute', bottom: height / 8, flexDirection: 'row' }}>
+        <Button text={"Log In"} />
+        <Button text={"Sign Up"} />
       </View>
       <Indicator scrollX={scrollx} />
     </View>
@@ -147,18 +115,44 @@ const styles = StyleSheet.create({
   },
   button: {
     borderRadius: 86,
-    width: width/3,
-    height: width/6,
+    width: width / 3,
+    height: width / 6,
     borderWidth: 2,
     margin: 10,
     borderColor: colours.SQUARE,
     alignItems: "center",
-    padding:20
+    justifyContent: 'center'
   },
-  buttonText: { 
+  buttonText: {
     fontWeight: '600',
-    fontSize: 20, 
-    color: colours.SQUARE, 
-    textAlign: 'center' 
+    fontSize: 18,
+    color: colours.SQUARE,
+    textAlign: 'center'
+  },
+  indicatorDot: {
+    height: 10,
+    width: 10,
+    borderRadius: 5,
+    backgroundColor: '#fff',
+    margin: 10,
+  },
+  square: {
+    width: height,
+    height: height,
+    backgroundColor: colours.SQUARE,
+    borderRadius: 86,
+    position: 'absolute',
+    top: -height * 0.6,
+    left: -height * 0.3
+  },
+  title:{ 
+    fontWeight: '800', 
+    fontSize: 28, 
+    marginBottom: 10, 
+    color: colours.SQUARE 
+  },
+  description:{ 
+    fontWeight: '300',
+    color: colours.SQUARE 
   }
 });
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { registerRootComponent } from 'expo';
 
-import LoginScreen from './LoginScreen';
+import OnboardingScreen from './OnboardingScreen';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(LoginScreen);
+registerRootComponent(OnboardingScreen);
